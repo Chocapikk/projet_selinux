333f7ca7e7e975049c6fe04974ed55136037b2b5
Chocapikk <88535377+Chocapikk@users.noreply.github.com>
2022-12-09 12:03:41 +0100

    Update selinux_policy.sh

--- selinux_policy.sh
+++ selinux_policy.sh
@@ -25,54 +25,9 @@ if [ "$1" == "install" ]; then
   # Activer SELinux en mode cible
   setenforce 1
   
-  # Créer un fichier .c 
-  cat << EOF > myprogram.c
-  #include <stdio.h>
-
-  int main(int argc, char *argv[])
-  {
-	  while(1) {
-		  FILE *fp;
-		  char buff[255];    
-		  fp = fopen("file.txt", "r");
-      fscanf(fp, "%s", buff);
-      printf("%s\\n", buff);
-      fclose(fp);
-	  }
-
-  return 0;
-  }
-  EOF
-  
   # Compiler le fichier myprogram.c
   gcc myprogram.c -o myprogram
   
-  # Créer un fichier .te
-  cat << EOF > mypolicy.te
-  policy_module(mypolicy, 1.0)
-
-  require {
-          type unconfined_t;
-          class file { read open };
-  }
-
-  type myprogram_t;
-
-  type myprogram_exec_t;
-
-  type myprogram_file_t;
-
-  init_module(mypolicy)
-
-  allow unconfined_t myprogram_t:file { open read };
-
-  allow myprogram_t myprogram_exec_t:file execute;
-
-  allow myprogram_t myprogram_file_t:file { open read };
-
-  files_type(myprogram_file_t)
-  EOF
-
   # Compiler le fichier .te
   make -f /usr/share/selinux/devel/Makefile mypolicy.pp
 
