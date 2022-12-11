cb5c9ab14ab033dcecbaa5b45a9703c63d7e341f
Chocapikk <88535377+Chocapikk@users.noreply.github.com>
2022-12-11 14:06:58 +0100

    Update myprogram.c

--- myprogram.c
+++ myprogram.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[])
-{
-	while(1) {
-		FILE *fp;
-		char buff[255];    
-		fp = fopen("file.txt", "r");
-    fscanf(fp, "%s", buff);
-    printf("%s\\n", buff);
-    fclose(fp);
-	}
+int main() {
+    FILE *fp;
+    char c;
 
-return 0;
+    while (1) {
+        fp = fopen("file.txt", "r");
+
+        if (fp == NULL) {
+            perror("Erreur d'ouverture du fichier");
+            return 1;
+        }
+
+        // Affichez le contenu du fichier ligne par ligne
+        while ((c = fgetc(fp)) != EOF) {
+            printf("%c", c);
+        }
+
+        fclose(fp);
+
+        // Attendez 1 seconde avant de recommencer la boucle
+        sleep(1.5);
+    }
 }
