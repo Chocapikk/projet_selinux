56886191b30d428e8edf916256ea4bc0a88ed91b
Chocapikk <88535377+Chocapikk@users.noreply.github.com>
2022-12-15 16:35:22 +0100

    Update selinux_policy2.sh

--- selinux_policy2.sh
+++ selinux_policy2.sh
@@ -7,54 +7,42 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
-# Vérifier si SELinux est déjà installé
-if ! dpkg -s selinux-utils > /dev/null 2>&1; then
-  # Installer SELinux
-  apt-get update
-  apt-get install selinux-utils
-fi
-
 # Vérifier si SEModule est déjà installé
-if ! dpkg -s policycoreutils > /dev/null 2>&1; then
+if ! yum list installed selinux-policy-devel > /dev/null 2>&1; then
   # Installer SEModule
-  apt-get update
-  apt-get install policycoreutils
-fi
-
-# Vérifier si SEModule est déjà installé
-if ! dpkg -s selinux-policy-dev > /dev/null 2>&1; then
-  # Installer SEModule
-  apt-get update
-  apt-get install selinux-policy-dev
+  yum update -y
+  yum install selinux-policy-devel -y
 fi
 
 # Vérifier si gcc est déjà installé
-if ! dpkg -s selinux-utils > /dev/null 2>&1; then
-  apt-get update
-  apt-get install gcc
+if ! yum list installed gcc > /dev/null 2>&1; then
+  yum update -y
+  yum install gcc -y
 fi
 
 # Vérifier si l'option "install" est spécifiée
 if [ "$1" == "install" ]; then
   # Activer SELinux en mode cible
   setenforce 1
-
+  # Créer le fichier texte "file.txt"
+  echo "Je suis un fichier texte" > file.txt
   # Compiler le fichier myprogram.c
   gcc myprogram.c -o myprogram
-
+  # Compiler le fichier myprogram.c dans wrong, celui ci n'aura pas le label infinite_loop_file_t
+  gcc myprogram.c -o wrong
   # Compiler le fichier .te
-  make -f /usr/share/selinux/devel/Makefile mypolicy.pp
-
+   make -f /usr/share/selinux/devel/Makefile infinite_loop.pp
   # Installer la politique
-  semodule -i mypolicy.pp
+  semodule -i infinite_loop.pp
 
-  # Assigner le type myprogram_t à notre programme
-  chcon -t myprogram_t myprogram
+  # Assigner le type infinite_loop_file_t à notre programme et au fichier texte
+  chcon -t mytype_t myprogram
+  runcon -t mytype_t myprogram
 
 fi
 
 # Vérifier si l'option "remove" est spécifiée
 if [ "$1" == "remove" ]; then
   # Enlever la politique
-  semodule -r mypolicy
+  semodule -r  infinite_loop
 fi
